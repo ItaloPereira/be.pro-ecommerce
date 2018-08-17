4d69cf7aa011973217f550da0bcc2c232ac541aa
Italo Pereira <italo.pereira@nodo.cc>
2018-08-17 17:58:55 -0300

    ...

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,39 +1,18 @@
 <template>
-    <section id="app">
-        <div class="nav-bar">
-            <div class="page-wrapper">
-                <div class="nav-part1">
-                    <span>Be.Pro</span>
-                    <ul class="nav-items1">
-                        <li>HOME</li>
-                        <li>SHOP</li>
-                        <li>PORTIFOLIO</li>
-                        <li>PAGE</li>
-                        <li>BLOG</li>
-                        <li>ELEMENTS</li>
-                    </ul>
-                </div>
-                <div class="nav-part2">
-                    <div class="nav-items2">
-                        <ul class="login">
-                            <li>LOG IN</li>
-                            <li>SIGN UP</li>
-                        </ul>
-                        <span class="bag icob-shopping-bag">
-                            <div class="items-number"><span>2</span></div>
-                        </span>
-                        <span class="search icob-search"></span>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <article id="app">
+        <Nav/>
         <router-view/>
-    </section>
+    </article>
 </template>
 
 <script>
+import Nav from './components/nav';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        Nav
+    }
 };
 </script>
 
